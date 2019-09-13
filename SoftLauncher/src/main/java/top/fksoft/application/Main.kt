f011866088f529e111354448759ea757c03155f6
Noob <ExplodingFKL@gmail.com>
2019-09-13 15:33:18 +0800

    Hello

---
+++ SoftLauncher/src/main/java/top/fksoft/application/Main.kt
@@ -0,0 +1,14 @@
+package top.fksoft.application
+
+/**
+ * @author Explo
+ */
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val readLine = readLine()
+        println(readLine)
+        kotlin.io.readLine()
+    }
+}
